cd83aaa2b80ee1ad73ba094d6de18f3c6ad25195
JediTiger <JediTiger@users.noreply.github.com>
2021-04-12 22:36:26 -0500

    Updates to scripts.js

--- scripts.js
+++ scripts.js
@@ -5,6 +5,11 @@ function init () {
     const landingButton = document.getElementById("landing");
     const abortButton = document.getElementById("missionAbort");
     const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    const buttonUp = document.getElementById("button-up");
+    const buttonDown = document.getElementById("button-down");
+    const buttonRight = document.getElementById("button-right");
+    const buttonLeft = document.getElementById("button-left");
+    
 // 2
 takeOffButton.addEventListener("click", function() {
   let liftoffAnswer = window.confirm("Confirm that the shuttle is ready for takeoff.")
@@ -13,7 +18,7 @@ takeOffButton.addEventListener("click", function() {
     console.log("Shuttle in flight.");
     shuttleBackground.style.background = "blue";
     console.log("Background color changed to blue");
-    spaceShuttleHeight.innerHTML = "10,000";
+    spaceShuttleHeight.innerHTML = 10000;
     console.log("Shuttle heigth set to 10,000 miles.")
   }
 });
@@ -29,8 +34,29 @@ landingButton.addEventListener("click", function() {
 });
 // 4
 abortButton.addEventListener("click", function() {
-  paragraph.innerHTML = "Houston, we have liftoff!";
-  console.log("Houston, we have liftoff!");    
+  let abortAnswer = window.confirm("Confirm that you want to abort the mission.");
+  if (abortAnswer) {
+    flightStatus.innerHTML = "Mission aborted.";
+    console.log("Mission aborted.");
+    shuttleBackground.style.background = "green";
+    console.log("Background color changed to green");
+    spaceShuttleHeight.innerHTML = "0";
+    console.log("Shuttle heigth set to 0 miles.");
+  }
+// 5
+/*
+	<button id="button-up"">Up</button>
+	<button id="button-down">Down</button>
+	<button id ="button-right">Right</button>
+	<button id="button-left">Left</button>
+*/
+buttonDown.addEventListener("click", function() {
+	spaceShuttleHeight.innerHTML = /*spaceShuttleHeight.innerHTML -*/ 10000;
+	console.log("Lower shuttle height by 10000");
+	
+});
+// Bonus
+//
 });
 }
 // 1
