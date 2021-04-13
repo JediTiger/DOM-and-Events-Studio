252fa954c367e521791fb7daf688eb1891d2ddf8
JediTiger <JediTiger@users.noreply.github.com>
2021-04-12 21:11:13 -0500

    Updates to scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ function init () {
     const shuttleBackground = document.getElementById("shuttleBackground");
     const takeOffButton = document.getElementById("takeoff");
     const landingButton = document.getElementById("landing");
-    const missionAbortButton = document.getElementById("missionAbort");
+    const abortButton = document.getElementById("missionAbort");
     const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
 // 2
 button.addEventListener("click", function() {
@@ -13,20 +13,22 @@ button.addEventListener("click", function() {
     console.log("Shuttle in flight.");
     shuttleBackground.backgroundColor = "blue";
     console.log("Background color changed to blue");
-    // somehow increase shuttle height by 10,000 miles
+    spaceShuttleHeight.innerHTML = 10000;
+    console.log("Shuttle heigth set to 10,000 miles.")
   }
 });
 // 3
-button.addEventListener("click", function() {
+landingButton.addEventListener("click", function() {
   window.alert("The shuttle is landing. Landing gear engaged.");
   flightStatus.innerHtml = "The shuttle has landed.";
   console.log("The shuttle has landed.");
   shuttleBackground.backgroundColor = "green";
   console.log("Background color changed to green");
-  // somehow reset shuttle height to 0
+  spaceShuttleHeight.innerHTML = 0;
+  console.log("Shuttle heigth set to 0 miles.")
 });
 // 4
-button.addEventListener("click", function() {
+abortButton.addEventListener("click", function() {
   paragraph.innerHTML = "Houston, we have liftoff!";
   console.log("Houston, we have liftoff!");    
 });
